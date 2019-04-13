6c2253673eb0a99e1dda8b33a5dd588fd4b11f55
MadEzra64 <49495334+MadEzra64@users.noreply.github.com>
2019-04-13 12:27:30 -0700

    Update 5

---
+++ ex14.py
@@ -0,0 +1,19 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+# Declaring our variables
+v1 = 0
+v2 = 0
+v3 = 0
+v4 = 0
+
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third varianle is:", third)
+
+print("Now let's occupy some more variables!")
+v1 = input("Type some data: ")
+v2 = input("Type a little bit more data please: ")
